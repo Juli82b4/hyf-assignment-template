00ec2446ee9df6720018fa69180bfe86a36eda96
juli82b4 <juliya.dian@gmail.com>
2026-01-06 22:36:49 +0100

    implement current year endpoint in exercise1.js

--- courses/foundation/intro-to-backend/exercise1.js
+++ courses/foundation/intro-to-backend/exercise1.js
@@ -7,8 +7,12 @@ app.get("/", (req, res) => {
   res.send("Hello from exercise 1!");
 });
 
+/*
+Respond with the current year
+*/
 app.get("/currentYear", (req, res) => {
-  // TODO: Implement this function to return a JSON object containing the current year
+  const currentYear = new Date().getFullYear();
+  res.json({ year: currentYear });
 });
 
 app.listen(port, () => {
