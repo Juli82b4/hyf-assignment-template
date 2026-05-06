aac2fd1c8b3cf7df0a294e963f99c1c86623d6be
juliya <juliya.dian@sriraj.com>
2026-05-06 20:37:50 +0200

    Refactor Navbar to use NavItem component and implement dynamic rendering of navbar links; update AddWishlistItem to handle custom input and thumbnail selection; enhance NasaCollaboration to fetch and display astronomy picture of the day.

--- courses/frontend/react/space-galactica-app/src/components/Navbar.jsx
+++ courses/frontend/react/space-galactica-app/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { NavItem } from "./NavItem.jsx";
 
 import styles from './Navbar.module.css';
 
@@ -31,26 +32,20 @@ export const Navbar = () => {
       <nav className={styles.navbar}>
         <div className={styles.navbarBG} />
         <ul className={styles.navbarList}>
-          {/* Task - Week 2 */}
+          {/* Task - Week 2 - this part has been deleted and replaced with map week 3 */}
           {/* Create a <NavItem> component, which accepts the following:  */}
-          {/* title, link, isActive  */}
-          <li className={classNames(styles.navbarLinks, {
-            [styles.isLinkActive]: navbarItems[0].link === currentPath,
-          })}>
-            <Link to={navbarItems[0].link}><b>01</b> {navbarItems[0].title}</Link>
-          </li>
-          <li className={classNames(styles.navbarLinks, {
-            [styles.isLinkActive]: navbarItems[1].link === currentPath,
-          })}>
-            <Link to={navbarItems[1].link}><b>02</b> {navbarItems[1].title}</Link>
-          </li>
-          <li className={classNames(styles.navbarLinks, {
-            [styles.isLinkActive]: navbarItems[2].link === currentPath,
-          })}>
-            <Link to={navbarItems[2].link}><b>03</b> NASA COLLABORATION</Link>
-          </li>
+          {/* title, link, isActive   */}
           {/* Task - Week 3 */}
           {/* replace repeating content by using navbarItems.map(() => <NavLink />) */}
+          {navbarItems.map((item, index) => (
+            <NavItem
+              key={item.link}
+              title={item.title}
+              link={item.link}
+              isActive={item.link === currentPath}
+              index={`0${index + 1}`}
+            />
+          ))}
         </ul>
       </nav>
     </header>
